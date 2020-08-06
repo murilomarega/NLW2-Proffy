b979a1cdb11af13113e69653cedea4b0bd22fec2
Murilo Marega <murilo.marega@ciag.org.br>
2020-08-06 17:23:21 -0300

    files lesson day 4, mobile interface

---
+++ mobile/src/pages/Favorites/styles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, // faz com que o objeto ocupe a tela toda
+        backgroundColor: '#f0f0f7',
+    },
+});
+
+export default styles;
